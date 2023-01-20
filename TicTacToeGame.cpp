503c8a323655adcf7500516686001e07216e308d
UtkarshaHolkar <103546232+UtkarshaHolkar@users.noreply.github.com>
2023-01-20 21:28:23 +0530

    Add files via upload

---
+++ TicTacToeGame.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+void board();
+int checkwin();
+int main()
+{
+    int player = 1; // it will decide which player is going to play and we have initialized by playerrrrr 1
+    int i, choice;
+    char mark;
+    do
+    {
+        board();
+        if (player % 3 == 1)
+        {
+            player = 1;
+        }
+        else
+        {
+            player = 3;
+        }
+        cout << "Player " << player << " Enter a number :" << endl;
+        cin >> choice;
+        if (player == 1)
+        {
+            mark = 'X';
+        }
+        else
+        {
+            mark = 'O';
+        }
+        if (choice == 1 && square[1] == '1')
+        {
+            square[1] = mark;
+        }
+        else if (choice == 2 && square[2] == '2')
+        {
+            square[2] = mark;
+        }
+        else if (choice == 3 && square[3] == '3')
+        {
+            square[3] = mark;
+        }
+        else if (choice == 4 && square[4] == '4')
+        {
+            square[4] = mark;
+        }
+        else if (choice == 5 && square[5] == '5')
+        {
+            square[5] = mark;
+        }
+        else if (choice == 6 && square[6] == '6')
+        {
+            square[6] = mark;
+        }
+        else if (choice == 7 && square[7] == '7')
+        {
+            square[7] = mark;
+        }
+        else if (choice == 8 && square[8] == '8')
+        {
+            square[8] = mark;
+        }
+        else if (choice == 9 && square[9] == '9')
+        {
+            square[9] = mark;
+        }
+        else
+        {
+            cout << "Oops..!! Invalid move " << endl;
+            player--;     // jo move player ne chala usko hum delt krdnge and usse lnge hi nhi
+            cin.ignore(); // it will be ignored by using ignore()
+            cin.get();    // and then again it will get another new number form user.
+        }
+        i = checkwin();
+        player++;
+    } while (i == -1);
+    board();
+    if (i == 1)
+    {
+        cout << " ==> \a Player " << --player << " Winss :) " << endl;
+    }
+    else
+    {
+        cout << "==> \a Game Draw !" << endl;
+    }
+    cin.ignore();
+    cin.get();
+    return 0;
+}
+
+void board()
+{
+    system("cls");
+    cout << "==========================================================================" << endl;
+    cout << " \t\t\t TIC TAC TOE GAME " << endl;
+    cout << "==========================================================================" << endl;
+    cout << endl;
+    cout << "Let's Play :)" << endl;
+    cout << endl;
+    cout << "Player1 [X] \n";
+    cout << "Player2 [O] \n";
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << " " << endl;
+    cout << "\t\t_____|_____|_____" << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << " " << endl;
+    cout << "\t\t_____|_____|_____" << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << " " << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << endl;
+}
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+    {
+        return 1;
+    }
+    else if (square[4] == square[5] && square[5] == square[6])
+    {
+        return 1;
+    }
+    else if (square[7] == square[8] && square[8] == square[9])
+    {
+        return 1;
+    }
+    else if (square[1] == square[4] && square[4] == square[7])
+    {
+        return 1;
+    }
+    else if (square[2] == square[5] && square[5] == square[8])
+    {
+        return 1;
+    }
+    else if (square[3] == square[6] && square[6] == square[9])
+    {
+        return 1;
+    }
+    else if (square[1] == square[5] && square[5] == square[9])
+    {
+        return 1;
+    }
+    else if (square[3] == square[5] && square[5] == square[7])
+    {
+        return 1;
+    }
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
